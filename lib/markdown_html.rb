3dcb9cd05dfeba462d66ccf1986e08b0f36f9456
Jonathan Rudenberg <jonathan@titanous.com>
2012-08-22 14:40:02 -0400

    Switch to albino for now, pygments.rb is segfaulting on heroku

--- lib/markdown_html.rb
+++ lib/markdown_html.rb
@@ -1,12 +1,10 @@
-require 'pygments.rb'
-
-RubyPython.start python_exe: 'python2.6' if ENV['RACK_ENV'] == 'production'
+require 'albino'
 
 class MarkdownHTML < Redcarpet::Render::HTML
   include Redcarpet::Render::SmartyPants
 
   def block_code(code, language)
-    Pygments.highlight(code, lexer: language)
+    language ? Albino.colorize(code, language) : code
   end
 
   def table(header, body)
