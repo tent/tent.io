beda980dec07c685fda35d309aa1b7f0bf34c19e
Jonathan Rudenberg <jonathan@titanous.com>
2012-08-22 11:39:39 -0400

    Fancy tables

--- lib/markdown_html.rb
+++ lib/markdown_html.rb
@@ -5,11 +5,22 @@ RubyPython.start python_exe: 'python2.6' if ENV['RACK_ENV'] == 'production'
 class MarkdownHTML < Redcarpet::Render::HTML
   include Redcarpet::Render::SmartyPants
 
-  def initialize(options={})
-    super options.merge(with_toc_data: true)
-  end
-
   def block_code(code, language)
     Pygments.highlight(code, lexer: language)
+  end
+
+  def table(header, body)
+    el('table', el('thead', header) + el('tbody', body), class: 'table table-striped table-bordered')
+  end
+
+  private
+
+  def el(el, content, attributes = {})
+    if content
+      attrs = attributes ? ' ' + attributes.map { |k,v| "#{k}=\"#{v}\"" }.join(' ') : ''
+      "<#{el}#{attrs}>\n#{content}</#{el}>\n"
+    else
+      ''
+    end
   end
 end
