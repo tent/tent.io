decb9d843023c0a827dda7862e768126f3bd764a
Jonathan Rudenberg <jonathan@titanous.com>
2012-08-21 23:22:02 -0400

    Add smartypants

--- lib/markdown_html.rb
+++ lib/markdown_html.rb
@@ -3,6 +3,8 @@ require 'pygments.rb'
 RubyPython.start python_exe: 'python2.6' if ENV['RACK_ENV'] == 'production'
 
 class MarkdownHTML < Redcarpet::Render::HTML
+  include Redcarpet::Render::SmartyPants
+
   def block_code(code, language)
     Pygments.highlight(code, lexer: language)
   end
